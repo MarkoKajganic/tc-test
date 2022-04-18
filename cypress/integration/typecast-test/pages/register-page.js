ea45e57d6b4ade6b4c90b391c3edc77d5aacfdf8
Marko <kajganicmarko@gmail.com>
2022-04-18 16:06:37 +0200

    first and last commit

---
+++ cypress/integration/typecast-test/pages/register-page.js
@@ -0,0 +1,116 @@
+export class Register {
+
+	genderFemale(){
+		return cy.get('#gender-female')
+	}
+
+	genderMale(){
+		return cy.get('#gender-male')
+	}
+
+	firstName(){
+		return cy.get('#FirstName')
+	}
+
+    lastName(){
+		return cy.get('#LastName')
+	}
+
+	dateDay(){
+		return cy.get('select[name="DateOfBirthDay"]')
+	}
+
+	dateMonth(){
+		return cy.get('select[name="DateOfBirthMonth"]')
+	}
+
+	dateYear(){
+		return cy.get('select[name="DateOfBirthYear"]')
+	}
+
+	email(){
+		return cy.get('#Email')
+	}
+
+	address(){
+		return cy.get('#StreetAddress')
+	}
+
+	zip(){
+		return cy.get('label[for="ZipPostalCode"]').next().next()
+	}
+
+	zipHelper(){
+		return cy.get('label[for="ZipPostalCode"]')
+	}
+
+	city(){
+		return cy.get('label[for="City"]').next().next().next()
+	}
+
+	country(){
+		return cy.get('#CountryId')
+	}
+
+	phone(){
+		return cy.get('#Phone')
+	}
+
+	newsletter(){
+		return cy.get('#Newsletter')
+	}
+
+	password(){
+		return cy.get('#Password')
+	}
+
+	passwordConfirmation(){
+		return cy.get('#ConfirmPassword')
+	}
+
+
+	//COMPANY ELEMENTS
+	registerAsCompany(){
+		return cy.get('#RegisterAsCompany')
+	}
+
+	companyName(){
+		return cy.get('#Company')
+	}
+
+	companyOIB(){
+		return cy.get('#CompanyOIB')
+	}
+
+	companyEmail(){
+		return cy.get('#CompanyEmail')
+	}
+
+	companyTelephone(){
+		return cy.get('#CompanyTelephone')
+	}
+
+	companyContactPerson(){
+		return cy.get('#CompanyContactPerson')
+	}
+
+	companyAddress(){
+		return cy.get('#CompanyAddress')
+	}
+
+	companyZip(){
+		return cy.get('label[for="CompanyZipPostalCode"]').next().next()
+	}
+
+	companyCity(){
+		return cy.get('label[for="CompanyCity"]').next().next().next()
+	}
+
+	companyCountry(){
+		return cy.get('#CompanyCountryId')
+	}
+
+	submit(){
+		return cy.get('#register-button')
+	}
+}
---
+++ cypress/integration/typecast-test/tc-test.js
@@ -0,0 +1,127 @@
+import {Register} from "./pages/register-page.js"
+const register = new Register()
+const { faker } = require('@faker-js/faker');
+
+
+describe('typecast happy flow ', () => {
+
+    before(() => {
+      cy.visit(Cypress.env('baseUrl'))
+    })
+
+    after(() => {
+      register.submit()
+              .click()
+    })
+
+
+    it('Basic info', () => {
+        register.genderFemale()
+                .click()
+                .should('have.value', 'F')
+       
+        register.genderMale()
+                .click()
+                .should('have.value', 'M')
+
+        register.firstName()
+                .type('Marko')
+       
+        register.lastName()
+                .type('Kajganic')
+
+        register.dateDay()
+                .select('1')
+        
+        register.dateMonth()
+                .select('8')
+        
+        register.dateYear()
+                .select('1989')
+
+        register.email()
+                .type(faker.internet.email())
+    })
+
+
+    it('Address', () => {
+        register.address()
+                .type('Novosadska 12')
+
+        register.zip()
+                .type('11000')
+        
+        register.zipHelper()
+                .click()    //click anywhere to select first and only option
+        
+        register.city()
+                .should('have.value', 'BEOGRAD')
+        
+        register.country()
+                .should('have.value', '85')
+    })
+
+
+    it('Phone and newsletter', () => {
+
+        register.phone()
+                .type('011 777 555')
+
+        register.newsletter()
+                .click()
+                .should('have.value', 'true')
+    })
+
+
+    it('PassWord', () => {
+
+        register.password()
+                .type('123456')
+
+        register.passwordConfirmation()
+                .type('123456')
+    })
+
+
+    it('Register as Company', () => {
+
+      register.registerAsCompany()
+              .click()
+              .should('have.value', 'true')
+
+      register.companyName()
+              .type('Ivina Tvrtka')
+      
+      register.companyOIB()
+              .type('12345678901')
+      
+      register.companyEmail()
+              .type(faker.internet.email())
+      
+      register.companyTelephone()
+              .type('011 555 777')
+      
+      register.companyContactPerson()
+              .type('Ivina Tetka')
+      
+      register.companyAddress()
+              .type('Novosadska 3')
+
+      register.companyZip()
+              .type('11000')
+
+      register.zipHelper()
+              .click()   
+      
+      register.companyCity()
+              .should('have.value', 'BEOGRAD');
+      
+      register.companyCountry()
+              .should('have.value', '85');
+    })
+})
+
+
+
+
+
